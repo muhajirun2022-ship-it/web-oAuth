bf6771b8a864e214bc6179ff7839b5732e182e83
muhajirun2022-ship-it <muhajirun2022@gmail.com>
2026-06-05 09:40:20 +0700

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -76,7 +76,7 @@ export default function Login() {
 
         <p className="login-note">
           Kami hanya menggunakan akun Google kamu untuk autentikasi.
-          Data kamu aman.
+          Data kamu aman. Web ini dibuat oleh Muhammad Ikhsan Kamil
         </p>
       </div>
     </div>
